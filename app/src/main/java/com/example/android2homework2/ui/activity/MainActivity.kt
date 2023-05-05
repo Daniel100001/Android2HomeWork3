f227e5a3cbd588920e4c18cb0611d2f27623226c
Daniel100001 <danldplayer2gmail.com>
2023-05-05 16:25:46 +0600

    It's Android 2 Home Work 3

--- app/src/main/java/com/example/android2homework2/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/android2homework2/ui/activity/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.android2homework2.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.example.android2homework2.R
+import com.example.android2homework2.databinding.ActivityMainBinding
+import com.example.android2homework2.utils.PreferenceHelper
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val preferenceHelper = PreferenceHelper()
+        preferenceHelper.unit(this)
+        val check = preferenceHelper.saveBoolean
+
+        if (check == false) {
+            navController.navigate(R.id.onBoardFragment)
+        } else {
+            navController.navigate(R.id.homeFragment)
+
+        }
     }
 }
--- app/src/main/java/com/example/android2homework2/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/android2homework2/ui/fragments/home/HomeFragment.kt
@@ -5,10 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.android2homework2.R
 import com.example.android2homework2.databinding.FragmentHomeBinding
-import com.example.android2homework2.utils.PreferenceHelper
 
 class HomeFragment : Fragment() {
 
@@ -21,24 +18,5 @@ class HomeFragment : Fragment() {
     ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
-
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        initialize()
-    }
-
-    private fun initialize() {
-        val preferenceHelper = PreferenceHelper()
-        preferenceHelper.unit(requireContext())
-        val check = preferenceHelper.saveBoolean
-
-        if (check == false) {
-            val navController = findNavController()
-            val navGraph = navController.navInflater.inflate(R.navigation.nav_graph)
-            navGraph.setStartDestination(R.id.onBoardFragment)
-            navController.graph = navGraph
-        }
     }
 }
